ac506e110a6a8d7786a4588d7f09a772d410ab3a
Mark Murray <mark.murray@shopify.com>
2026-04-06 08:49:13 +0100

    Versioning

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,5 +1,7 @@
 /// <reference types="vite/client" />
 
+declare const __APP_VERSION__: string
+
 declare module "@meronex/icons/*" {
   import { ComponentType } from "react"
   const Icon: ComponentType<Record<string, unknown>>
